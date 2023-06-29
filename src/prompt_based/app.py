40111eee72a2a9e56f3527b0c7373efe3bb615a7
melihunsal <melih@synthdata.co>
2023-06-29 23:29:12 +0300

    streamlit app draft started

--- src/prompt_based/app.py
+++ src/prompt_based/app.py
@@ -22,63 +22,45 @@ st.title(title)
 
 openai_api_key = st.text_input('Enter your OpenAI API Key', placeholder='sk-...',value=os.getenv('OPENAI_API_KEY',''),type="password")
 demo_title = st.text_input('Enter your demo title', placeholder='Type your demo title')
-empty_idea = st.empty()
-demo_idea = empty_idea.text_area('Enter your LLM-based demo idea', placeholder = 'Type your demo idea here', height=100)
-
 
 st.write("Examples")
 
-
 cols = st.columns([1,1,1.2])
-
-
 
 examples = ["Language Translator 📝","Grammer Corrector 🛠","Blog post generator from title 📔"] 
 
 pid = None
 
-example_submitted = False
+submitted = False
 
-with st.form('a', clear_on_submit=True):
-    submitted = st.form_submit_button('Submit')
-    for col,example in zip(cols,examples):
-        if col.button(example):
-            example_submitted = True
-            demo_idea = empty_idea.text_area('Enter your LLM-based demo idea', example, height=100)
-            print(demo_idea)
+for col,example in zip(cols,examples):
+    if col.button(example):
+        submitted = True
+        demo_idea = example[:-1]
+        print(demo_idea)
 
-    if submitted or example_submitted:
 
-        if not openai_api_key:
-            st.warning('Please enter your OpenAI API Key', icon="⚠️")
+if not openai_api_key:
+    st.warning('Please enter your OpenAI API Key', icon="⚠️")
+else:
+    bar = st.progress(25, "Generating Code...")
+    for data in generate_response(demo_idea):
+        response = data["out"]
+        error = data["error"]
+        code = data["code"]
+        test_code = data["test_code"]
+        success = data["success"]
+        percentage = data["percentage"]
+
+        if success:
+            bar.progress(75, text="Creating App...")
+            example_submitted = False
+            sleep(4)
+            webbrowser.open('http://localhost:8502')
+
         else:
-            agent = LogicModel(openai_api_key=openai_api_key)
-            streamlit_agent = StreamlitModel(openai_api_key=openai_api_key)
+            bar.progress(50, text="Refining Code...")
 
-            if pid:
-                print("Terminating...")
-                os.kill(pid, signal.SIGTERM)
-                pid = None
-            
-            bar = st.progress(25, "Generating Code...")
-            for data in generate_response(demo_idea):
-                response = data["out"]
-                error = data["error"]
-                code = data["code"]
-                test_code = data["test_code"]
-                success = data["success"]
-                percentage = data["percentage"]
-
-                if success:
-                    bar.progress(75, text="Creating App...")
-                    example_submitted = False
-                    pid = streamlit_agent(demo_idea,demo_title,code,test_code,bar.progress,st.balloons)
-                    sleep(4)
-                    webbrowser.open('http://localhost:8502')
-
-                else:
-                    bar.progress(50, text="Refining Code...")
-
-                if success:
-                    #st.balloons()
-                    break
+        if success:
+            #st.balloons()
+            break
