8b5681e25a234f81298052a778c6a772e599ee68
wusiyao <wusiyao@neusoft.com>
2023-09-04 14:21:04 +0800

    update config

--- demogpt/model_config.py
+++ demogpt/model_config.py
@@ -10,12 +10,14 @@ logging.basicConfig(format=LOG_FORMAT)
 llm_model_dict = {
     "chatglm-6b": {
         "model_name": "chatglm-6b",
+        "local_model_path": "/opt/ChatGLM-6B/chatglm-6b/",
         "api_base_url": "http://localhost:8888/v1",  # "name"修改为fastchat服务中的"api_base_url"
         "api_key": "EMPTY"
     },
 
     "chatglm2-6b": {
         "model_name": "chatglm2-6b",
+        "local_model_path": "/opt/ChatGLM2-6B/chatglm2-6b/",
         "api_base_url": "http://localhost:8888/v1",  # "name"修改为fastchat服务中的"api_base_url"
         "api_key": "EMPTY"
     },
@@ -35,9 +37,17 @@ llm_model_dict = {
 
     "baichuan-7b": {
         "model_name": "baichuan-7b",
+        "local_model_path": "/opt/baichuan-7B",
         "api_base_url": "http://localhost:8888/v1",  # "name"修改为fastchat服务中的"api_base_url"
         "api_key": "EMPTY"
     },
+    "Baichuan-13b-Chat": {
+        "model_name": "baichuan-13b",
+        "local_model_path": "baichuan-inc/Baichuan-13b-Chat",
+        "api_base_url": "http://localhost:8888/v1",  # "name"修改为fastchat服务中的"api_base_url"
+        "api_key": "EMPTY"
+    },
+
 }
 
 # LLM 名称
