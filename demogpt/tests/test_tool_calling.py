f6dfd252feb2fac5eb0029d6abd55f05e143beb3
Melih Ünsal <06melihunsal@gmail.com>
2024-10-20 16:30:35 +0300

    test query updated

--- demogpt/tests/test_tool_calling.py
+++ demogpt/tests/test_tool_calling.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     search_tool = DuckDuckGoSearchTool()
     weather_tool = WeatherTool()
     assistant = ToolCallingAssistant(tools=[search_tool, weather_tool], llm=OpenAIChatModel(model_name="gpt-4o-mini"), verbose=True)
-    query = "Who is Daron Acemoglu?"
+    query = "Who has won the Nobel Prize in Economics in 2024?"
     assistant.ask(query)
 
